2b7573c003321370d10f6e046751aa3cf2922781
Anders Ingemann <anders@ingemann.de>
2019-05-05 12:51:48 +0200

    Do not reset args & left on every optional non-match
    That would also reset previous matches. There is in fact no need
    to reset those two values at all, since any subtree elements would
    either not modify them when not match is found (leaves) or
    reset the to their original state (branches)

--- lib/branches/optional.sh
+++ lib/branches/optional.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 optional() {
-  local initial_left=("${left[@]}")
-  # shellcheck disable=SC2154
-  local reset_from=${#params_set[@]}
   local pattern
   for pattern in "$@"; do
-    if ! $pattern; then
-      left=("${initial_left[@]}")
-      unset_params "${params_set[@]:$reset_from}"
-    fi
+    $pattern
   done
   return 0
 }
