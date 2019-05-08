a880f5212123438be059b43d387646d98a3a2158
Anders Ingemann <anders@ingemann.de>
2019-05-08 20:07:50 +0200

    test: Add test for patching a physical file

--- tests/test_scriptpatching.py
+++ tests/test_scriptpatching.py
@@ -1,4 +1,9 @@
+import os
+import sys
+import subprocess
+from tempfile import NamedTemporaryFile
 from . import patched_script
+from docopt_sh.__main__ import main as docopt_sh_main
 
 def test_arg(monkeypatch, capsys):
   with patched_script(monkeypatch, capsys, 'echo_ship_name.sh') as run:
@@ -22,3 +27,21 @@ def test_wrong_usage(monkeypatch, capsys):
   with patched_script(monkeypatch, capsys, 'echo_ship_name.sh') as run:
     process = run('--bad-opt')
     assert process.returncode != 0
+
+def test_patch_file(monkeypatch):
+  with open('tests/scripts/echo_ship_name.sh') as h:
+    script = h.read()
+  file = NamedTemporaryFile(mode='w', delete=False)
+  try:
+    file.write(script)
+    file.close()
+    with monkeypatch.context() as m:
+      m.setattr(sys, 'argv', ['docopt.sh', file.name])
+      docopt_sh_main()
+      process = subprocess.run(
+        ['bash', file.name, 'ship', 'new', 'Olympia'],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE
+      )
+      assert process.stdout.decode('utf-8') == 'Olympia\n'
+  finally:
+    os.unlink(file.name)
