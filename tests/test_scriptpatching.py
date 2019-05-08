8cc6687045a3c388e7983f30d92a9c6e64645708
Anders Ingemann <anders@ingemann.de>
2019-05-08 22:21:48 +0200

    tests: Test --no-teardown

--- tests/test_scriptpatching.py
+++ tests/test_scriptpatching.py
@@ -1,4 +1,7 @@
-from . import patched_script, invoke_docopt, temp_script
+import re
+from io import StringIO
+from . import bash_eval_script, patched_script, invoke_docopt, temp_script
+from docopt_sh.patcher import get_doc
 
 def test_arg(monkeypatch, capsys):
   with patched_script(monkeypatch, capsys, 'echo_ship_name.sh') as run:
@@ -58,7 +61,6 @@ def test_patch_file(monkeypatch):
     assert out == 'Olympia\n'
 
 def test_doc_check(monkeypatch):
-  import re
   with temp_script('echo_ship_name.sh') as (script, run):
     invoke_docopt(monkeypatch, params=[script.name])
     with open(script.name, 'r') as h:
@@ -70,7 +72,6 @@ def test_doc_check(monkeypatch):
     assert re.match('^The current usage doc \([^)]+\) does not match what the parser was generated with \([^)]+\)\n$', err) is not None
 
 def test_no_doc_check(monkeypatch):
-  import re
   with temp_script('echo_ship_name.sh') as (script, run):
     invoke_docopt(monkeypatch, params=['--no-doc-check', script.name])
     with open(script.name, 'r') as h:
@@ -82,21 +83,46 @@ def test_no_doc_check(monkeypatch):
     assert out == 'Olympia\n'
 
 def test_only_parser(monkeypatch, capsys):
-  from docopt_sh.patcher import get_doc
-  from . import bash_eval_script
-  from io import StringIO
   with open('tests/scripts/naval_fate.sh') as h:
     script = h.read()
-    doc = get_doc(script)[0]
-    h.seek(0)
-    parser = invoke_docopt(monkeypatch, capsys=capsys, params=['--only-parser'], stdin=h).out
-    program = '''
+  doc = get_doc(script)[0]
+  parser = invoke_docopt(monkeypatch, capsys=capsys, params=['--only-parser'], stdin=StringIO(script)).out
+  program = '''
 doc="{doc}"
 {parser}
 docopt "$@"
 echo $((__x_ + __y_))
 '''.format(doc=doc, parser=parser)
-    captured = invoke_docopt(monkeypatch, capsys, stdin=StringIO(program))
-    code, out, err = bash_eval_script(captured.out, ['ship', 'shoot', '3', '1'])
-    assert code == 0
-    assert out == '4\n'
+  captured = invoke_docopt(monkeypatch, capsys, stdin=StringIO(program))
+  code, out, err = bash_eval_script(captured.out, ['ship', 'shoot', '3', '1'])
+  assert code == 0
+  assert out == '4\n'
+
+def test_teardown(monkeypatch, capsys):
+  with open('tests/scripts/naval_fate.sh') as h:
+    doc = get_doc(h.read())[0]
+  program = '''
+doc="{doc}"
+docopt "$@"
+echo ${{parsed_params[0]}}
+echo ${{parsed_values[0]}}
+'''.format(doc=doc)
+  program = invoke_docopt(monkeypatch, capsys=capsys, stdin=StringIO(program)).out
+  code, out, err = bash_eval_script(program, ['ship', 'shoot', '3', '1'])
+  assert err == ''
+  assert code == 0
+  assert out == '\n\n'
+
+def test_no_teardown(monkeypatch, capsys):
+  with open('tests/scripts/naval_fate.sh') as h:
+    doc = get_doc(h.read())[0]
+  program = '''
+doc="{doc}"
+docopt "$@"
+echo ${{parsed_params[@]}}
+echo ${{parsed_values[@]}}
+'''.format(doc=doc)
+  program = invoke_docopt(monkeypatch, capsys=capsys, params=['--no-teardown'], stdin=StringIO(program)).out
+  code, out, err = bash_eval_script(program, ['ship', 'shoot', '3', '1'])
+  assert code == 0
+  assert out == 'a a a a\nship shoot 3 1\n'
