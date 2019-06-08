46fb996b2ac0ada1a4a6a4742a3807dbc2d802b1
Anders Ingemann <anders@ingemann.de>
2019-06-08 23:15:25 +0200

    Implement some useful warnings and errors for the user

--- docopt_sh/script.py
+++ docopt_sh/script.py
@@ -10,40 +10,46 @@ class Script(object):
     self.contents = contents
     self.path = path
 
-    self.doc = Doc(self.contents)
-    self.parser = Parser(self.contents, self.doc)
-    self.invocation = Invocation(self.contents, self.parser)
-    self.options = [Option(name, self.contents) for name in [
+    self.doc = Doc(self)
+    self.parser = Parser(self, self.doc)
+    self.invocation = Invocation(self, self.parser)
+    self.options = [Option(self, name) for name in [
       'docopt_program_version', 'docopt_add_help', 'docopt_options_first',
       'docopt_teardown', 'docopt_doc_check', 'docopt_lib_check'
     ]]
 
   def validate_script_locations(self):
     if not self.doc.present:
-      raise DocoptScriptValidationError('Variable containing usage doc not found.', self.path)
+      raise DocoptScriptValidationError('Could not find variable containing usage doc.', self.doc)
     if self.doc.count > 1:
-      raise DocoptScriptValidationError('More than one variable contain usage doc found.', self.path)
+      raise DocoptScriptValidationError('More than one variable containing usage doc found.', self.doc)
     if self.parser.start_guard.count > 1:
-      raise DocoptScriptValidationError('Multiple docopt parser start guards found', self.path)
+      raise DocoptScriptValidationError('Multiple docopt parser start guards found', self.parser.start_guard)
     if self.parser.end_guard.count > 1:
-      raise DocoptScriptValidationError('Multiple docopt parser end guards found', self.path)
+      raise DocoptScriptValidationError('Multiple docopt parser end guards found', self.parser.end_guard)
     if self.parser.start_guard.present and not self.parser.end_guard.present:
-        raise DocoptScriptValidationError('Parser begin guard found, but no end guard detected', self.path)
+        raise DocoptScriptValidationError(
+          'Parser begin guard found, but no end guard detected', self.parser.start_guard)
     if self.parser.end_guard.present and not self.parser.start_guard.present:
-      raise DocoptScriptValidationError('Parser end guard found, but no begin guard detected', self.path)
+      raise DocoptScriptValidationError(
+        'Parser end guard found, but no begin guard detected', self.parser.end_guard)
     if self.invocation.count > 1:
-      log.warning('Multiple invocations of docopt found, check your script to make sure this is correct.')
+      log.warning(
+        '%s Multiple invocations of docopt found, check your script to make sure this is correct.',
+        self.invocation
+      )
     if not self.invocation.present:
       log.warning(
-        'No invocations of docopt found, check your script to make sure this is correct.\n'
-        'docopt.sh is invoked with `docopt "$@"`'
+        '%s No invocations of docopt found, check your script to make sure this is correct.\n'
+        'docopt.sh is invoked with `docopt "$@"`',
+        self.invocation
       )
     for option in self.options:
-      if option.present and option.start > self.invocation.end:
+      if option.present and option.start > self.invocation.last.end:
         log.warning(
-          '$%s has no effect when specified after invoking docopt, '
+          '%s $%s has no effect when specified after invoking docopt, '
           'make sure to place docopt options before calling `docopt "$@"`.',
-          option.name
+          option, option.name
         )
 
   def patch(self, parser):
@@ -66,13 +72,22 @@ class Script(object):
 
 class ScriptLocation(object):
 
-  def __init__(self, matches, offset):
-    self.matches = matches
-    self.match = next(matches, None)
+  def __init__(self, script, matches, offset):
+    self.script = script
+    self.matches = list(matches)
+    self.match = self.matches[0] if self.matches else None
     self.offset = 0 if offset is None else offset
 
   def __len__(self):
     return self.end - self.start if self.present else 0
+
+  @property
+  def all(self):
+    return [ScriptLocation(self.script, iter([match]), self.offset) for match in self.matches]
+
+  @property
+  def last(self):
+    return ScriptLocation(self.script, iter([self.matches[-1]]), self.offset)
 
   @property
   def present(self):
@@ -80,7 +95,7 @@ class ScriptLocation(object):
 
   @property
   def count(self):
-    return len(list(self.matches))
+    return len(self.matches)
 
   @property
   def start(self):
@@ -92,16 +107,28 @@ class ScriptLocation(object):
     if self.present:
       return self.match.end(0) + self.offset
 
+  @property
+  def line(self):
+    return self.script.contents[:self.start].count('\n') + 1
+
+  def __str__(self):
+    if not self.present:
+      return '%s' % self.script.path
+    if self.count > 1:
+      return '%s:%s' % (self.script.path, ','.join(map(lambda l: str(l.line), self.all)))
+    else:
+      return '%s:%d' % (self.script.path, self.line)
+
 
 class Doc(ScriptLocation):
 
   def __init__(self, script):
     matches = re.finditer(
-      r'([a-zA-Z_][a-zA-Z_0-9]*)="(\s*)(.*?Usage:.+?)(\s*)"(\n|;)',
-      script,
+      r'([a-zA-Z_][a-zA-Z_0-9]*)="(\s*)(.*?\bUsage:.+?)(\s*)"(\n|;)',
+      script.contents,
       re.MULTILINE | re.IGNORECASE | re.DOTALL
     )
-    super(Doc, self).__init__(matches, 0)
+    super(Doc, self).__init__(script, matches, 0)
 
   @property
   def name(self):
@@ -121,15 +148,15 @@ class Doc(ScriptLocation):
 class ParserStartGuard(ScriptLocation):
 
   def __init__(self, script, doc):
-    matches = re.finditer(r'# docopt parser below[^\n]*\n', script[doc.end:], re.MULTILINE)
-    super(ParserStartGuard, self).__init__(matches, doc.end)
+    matches = re.finditer(r'# docopt parser below[^\n]*\n', script.contents[doc.end:], re.MULTILINE)
+    super(ParserStartGuard, self).__init__(script, matches, doc.end)
 
 
 class ParserEndGuard(ScriptLocation):
 
   def __init__(self, script, start_guard):
-    matches = re.finditer(r'# docopt parser above[^\n]*\n', script[start_guard.end:], re.MULTILINE)
-    super(ParserEndGuard, self).__init__(matches, start_guard.end)
+    matches = re.finditer(r'# docopt parser above[^\n]*\n', script.contents[start_guard.end:], re.MULTILINE)
+    super(ParserEndGuard, self).__init__(script, matches, start_guard.end)
 
 
 class Parser(object):
@@ -167,16 +194,16 @@ class Parser(object):
 class Invocation(ScriptLocation):
 
   def __init__(self, script, parser):
-    matches = re.finditer(r'docopt\s+"\$\@"', script[parser.end:])
-    super(Invocation, self).__init__(matches, parser.end)
+    matches = re.finditer(r'docopt\s+"\$\@"', script.contents[parser.end:])
+    super(Invocation, self).__init__(script, matches, parser.end)
 
 
 class Option(ScriptLocation):
 
-  def __init__(self, name, script):
+  def __init__(self, script, name):
     self.name = name
-    matches = re.finditer(r'^%s=' % name, script, re.MULTILINE)
-    super(Option, self).__init__(matches, 0)
+    matches = re.finditer(r'^%s=' % name, script.contents, re.MULTILINE)
+    super(Option, self).__init__(script, matches, 0)
     if self.count > 1:
       # Override parent class selection of first match, previous assignments
       # would be overwritten so it's the last match that has an effect
@@ -185,12 +212,12 @@ class Option(ScriptLocation):
 
 class DocoptScriptValidationError(Exception):
 
-  def __init__(self, message, path=None):
+  def __init__(self, message, script_location=None):
     super(DocoptScriptValidationError, self).__init__(message)
     self.message = message
-    self.path = path
+    self.script_location = script_location
 
   def __str__(self):
-    if self.path:
-      return 'Error in %s: %s' % (self.path, self.message)
+    if self.script_location is not None:
+      return '%s %s' % (self.script_location, self.message)
     return self.message
