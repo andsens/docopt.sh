15f28e70ba5207f83a746a2a533cd3cdb3fe042f
Anders Ingemann <anders@ingemann.de>
2019-06-10 09:44:57 +0200

    bump version and point pyproject.toml at README.rst

--- docopt_sh/__init__.py
+++ docopt_sh/__init__.py
@@ -7,7 +7,7 @@ docopt.sh - Bash argument parser generator.
 # in order to prevent docopt from parsing it as a `usage:`` section
 
 __all__ = ['docopt_sh']
-__version__ = '0.9.1'
+__version__ = '0.9.5'
 
 
 class DocoptError(Exception):
