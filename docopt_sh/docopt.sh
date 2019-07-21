220e68e5aa8bd9394c00deb7c4e97ae7f8b94fea
Anders Ingemann <anders@ingemann.de>
2019-07-21 23:19:32 +0200

    Reorder functions

--- docopt_sh/docopt.sh
+++ docopt_sh/docopt.sh
@@ -58,130 +58,82 @@ does not match the version of the invoking docopt parser (%s)\nEOM\nexit 70\n" \
 fi
 }
 
-either() {
-  local initial_left=("${left[@]}")
-  local best_match_idx
-  local match_count
-  local node_idx
-  ((testdepth++)) || true
-  for node_idx in "$@"; do
-    if "node_$node_idx"; then
-      if [[ -z $match_count || ${#left[@]} -lt $match_count ]]; then
-        best_match_idx=$node_idx
-        match_count=${#left[@]}
+parse() {
+  if ${DOCOPT_DOC_CHECK:-true}; then
+    local doc_hash
+    doc_hash=$(printf "%s" "$DOC" | shasum -a 256)
+    if [[ ${doc_hash:0:5} != "$digest" ]]; then
+      stderr "The current usage doc (${doc_hash:0:5}) does not match \
+what the parser was generated with (${digest})
+Run \`docopt.sh\` to refresh the parser."
+      _return 70
+    fi
+  fi
+
+  local root_idx=$1
+  shift
+  argv=("$@")
+  parsed_params=()
+  parsed_values=()
+  left=()
+  # testing depth counter, when >0 nodes only check for potential matches
+  # when ==0 leafs will set the actual variable when a match is found
+  testdepth=0
+
+  local arg
+  while [[ ${#argv[@]} -gt 0 ]]; do
+    if [[ ${argv[0]} = "--" ]]; then
+      for arg in "${argv[@]}"; do
+        parsed_params+=('a')
+        parsed_values+=("$arg")
+      done
+      break
+    elif [[ ${argv[0]} = --* ]]; then
+      parse_long
+    elif [[ ${argv[0]} = -* && ${argv[0]} != "-" ]]; then
+      parse_shorts
+    elif ${DOCOPT_OPTIONS_FIRST:-false}; then
+      for arg in "${argv[@]}"; do
+        parsed_params+=('a')
+        parsed_values+=("$arg")
+      done
+      break
+    else
+      parsed_params+=('a')
+      parsed_values+=("${argv[0]}")
+      argv=("${argv[@]:1}")
+    fi
+  done
+  local idx
+  if ${DOCOPT_ADD_HELP:-true}; then
+    for idx in "${parsed_params[@]}"; do
+      [[ $idx = 'a' ]] && continue
+      if [[ ${shorts[$idx]} = "-h" || ${longs[$idx]} = "--help" ]]; then
+        stdout "$doc"
+        _return 0
       fi
-    fi
-    left=("${initial_left[@]}")
-  done
-  ((testdepth--)) || true
-  if [[ -n $best_match_idx ]]; then
-    "node_$best_match_idx"
-    return 0
-  fi
-  left=("${initial_left[@]}")
-  return 1
-}
-
-oneormore() {
-  local i=0
-  local prev=${#left[@]}
-  while "node_$1"; do
-    ((i++)) || true
-    [[ $prev -eq ${#left[@]} ]] && break
-    prev=${#left[@]}
-  done
-  if [[ $i -ge 1 ]]; then
-    return 0
-  fi
-  return 1
-}
-
-optional() {
-  local node_idx
-  for node_idx in "$@"; do
-    "node_$node_idx"
-  done
-  return 0
-}
-
-required() {
-  local initial_left=("${left[@]}")
-  local node_idx
-  ((testdepth++)) || true
-  for node_idx in "$@"; do
-    if ! "node_$node_idx"; then
-      left=("${initial_left[@]}")
-      ((testdepth--)) || true
-      return 1
-    fi
-  done
-  if [[ $((--testdepth)) -eq 0 ]]; then
-    left=("${initial_left[@]}")
-    for node_idx in "$@"; do
-      "node_$node_idx"
     done
   fi
+  if [[ ${DOCOPT_PROGRAM_VERSION:-false} != 'false' ]]; then
+    for idx in "${parsed_params[@]}"; do
+      [[ $idx = 'a' ]] && continue
+      if [[ ${longs[$idx]} = "--version" ]]; then
+        stdout "$DOCOPT_PROGRAM_VERSION"
+        _return 0
+      fi
+    done
+  fi
+
+  local i=0
+  while [[ $i -lt ${#parsed_params[@]} ]]; do
+    left+=("$i")
+    ((i++)) || true
+  done
+
+  if ! required "$root_idx" || [ ${#left[@]} -gt 0 ]; then
+    error
+  fi
   return 0
-}
-
-switch() {
-  local i
-  for i in "${!left[@]}"; do
-    local l=${left[$i]}
-    if [[ ${parsed_params[$l]} = "$2" ]]; then
-      left=("${left[@]:0:$i}" "${left[@]:((i+1))}")
-      [[ $testdepth -gt 0 ]] && return 0
-      if [[ $3 = true ]]; then
-        eval "((var_$1++))" || true
-      else
-        eval "var_$1=true"
-      fi
-      return 0
-    fi
-  done
-  return 1
-}
-
-value() {
-  local i
-  for i in "${!left[@]}"; do
-    local l=${left[$i]}
-    if [[ ${parsed_params[$l]} = "$2" ]]; then
-      left=("${left[@]:0:$i}" "${left[@]:((i+1))}")
-      [[ $testdepth -gt 0 ]] && return 0
-      local value
-      value=$(printf -- "%q" "${parsed_values[$l]}")
-      if [[ $3 = true ]]; then
-        eval "var_$1+=($value)"
-      else
-        eval "var_$1=$value"
-      fi
-      return 0
-    fi
-  done
-  return 1
-}
-
-_command() {
-  local i
-  local name=${2:-$1}
-  for i in "${!left[@]}"; do
-    local l=${left[$i]}
-    if [[ ${parsed_params[$l]} = 'a' ]]; then
-      if [[ ${parsed_values[$l]} != "$name" ]]; then
-        return 1
-      fi
-      left=("${left[@]:0:$i}" "${left[@]:((i+1))}")
-      [[ $testdepth -gt 0 ]] && return 0
-      if [[ $3 = true ]]; then
-        eval "((var_$1++)) || true"
-      else
-        eval "var_$1=true"
-      fi
-      return 0
-    fi
-  done
-  return 1
 }
 
 parse_shorts() {
@@ -296,6 +248,132 @@ parse_long() {
   parsed_values+=("$value")
 }
 
+required() {
+  local initial_left=("${left[@]}")
+  local node_idx
+  ((testdepth++)) || true
+  for node_idx in "$@"; do
+    if ! "node_$node_idx"; then
+      left=("${initial_left[@]}")
+      ((testdepth--)) || true
+      return 1
+    fi
+  done
+  if [[ $((--testdepth)) -eq 0 ]]; then
+    left=("${initial_left[@]}")
+    for node_idx in "$@"; do
+      "node_$node_idx"
+    done
+  fi
+  return 0
+}
+
+either() {
+  local initial_left=("${left[@]}")
+  local best_match_idx
+  local match_count
+  local node_idx
+  ((testdepth++)) || true
+  for node_idx in "$@"; do
+    if "node_$node_idx"; then
+      if [[ -z $match_count || ${#left[@]} -lt $match_count ]]; then
+        best_match_idx=$node_idx
+        match_count=${#left[@]}
+      fi
+    fi
+    left=("${initial_left[@]}")
+  done
+  ((testdepth--)) || true
+  if [[ -n $best_match_idx ]]; then
+    "node_$best_match_idx"
+    return 0
+  fi
+  left=("${initial_left[@]}")
+  return 1
+}
+
+optional() {
+  local node_idx
+  for node_idx in "$@"; do
+    "node_$node_idx"
+  done
+  return 0
+}
+
+oneormore() {
+  local i=0
+  local prev=${#left[@]}
+  while "node_$1"; do
+    ((i++)) || true
+    [[ $prev -eq ${#left[@]} ]] && break
+    prev=${#left[@]}
+  done
+  if [[ $i -ge 1 ]]; then
+    return 0
+  fi
+  return 1
+}
+
+_command() {
+  local i
+  local name=${2:-$1}
+  for i in "${!left[@]}"; do
+    local l=${left[$i]}
+    if [[ ${parsed_params[$l]} = 'a' ]]; then
+      if [[ ${parsed_values[$l]} != "$name" ]]; then
+        return 1
+      fi
+      left=("${left[@]:0:$i}" "${left[@]:((i+1))}")
+      [[ $testdepth -gt 0 ]] && return 0
+      if [[ $3 = true ]]; then
+        eval "((var_$1++)) || true"
+      else
+        eval "var_$1=true"
+      fi
+      return 0
+    fi
+  done
+  return 1
+}
+
+switch() {
+  local i
+  for i in "${!left[@]}"; do
+    local l=${left[$i]}
+    if [[ ${parsed_params[$l]} = "$2" ]]; then
+      left=("${left[@]:0:$i}" "${left[@]:((i+1))}")
+      [[ $testdepth -gt 0 ]] && return 0
+      if [[ $3 = true ]]; then
+        eval "((var_$1++))" || true
+      else
+        eval "var_$1=true"
+      fi
+      return 0
+    fi
+  done
+  return 1
+}
+
+value() {
+  local i
+  for i in "${!left[@]}"; do
+    local l=${left[$i]}
+    if [[ ${parsed_params[$l]} = "$2" ]]; then
+      left=("${left[@]:0:$i}" "${left[@]:((i+1))}")
+      [[ $testdepth -gt 0 ]] && return 0
+      local value
+      value=$(printf -- "%q" "${parsed_values[$l]}")
+      if [[ $3 = true ]]; then
+        eval "var_$1+=($value)"
+      else
+        eval "var_$1=$value"
+      fi
+      return 0
+    fi
+  done
+  return 1
+}
+
 stdout() {
   printf -- "cat <<'EOM'\n%s\nEOM\n" "$1"
 }
@@ -315,80 +393,3 @@ _return() {
   exit "$1"
 }
 
-parse() {
-  if ${DOCOPT_DOC_CHECK:-true}; then
-    local doc_hash
-    doc_hash=$(printf "%s" "$DOC" | shasum -a 256)
-    if [[ ${doc_hash:0:5} != "$digest" ]]; then
-      stderr "The current usage doc (${doc_hash:0:5}) does not match \
-what the parser was generated with (${digest})
-Run \`docopt.sh\` to refresh the parser."
-      _return 70
-    fi
-  fi
-
-  local root_idx=$1
-  shift
-  argv=("$@")
-  parsed_params=()
-  parsed_values=()
-  left=()
-  # testing depth counter, when >0 nodes only check for potential matches
-  # when ==0 leafs will set the actual variable when a match is found
-  testdepth=0
-
-  local arg
-  while [[ ${#argv[@]} -gt 0 ]]; do
-    if [[ ${argv[0]} = "--" ]]; then
-      for arg in "${argv[@]}"; do
-        parsed_params+=('a')
-        parsed_values+=("$arg")
-      done
-      break
-    elif [[ ${argv[0]} = --* ]]; then
-      parse_long
-    elif [[ ${argv[0]} = -* && ${argv[0]} != "-" ]]; then
-      parse_shorts
-    elif ${DOCOPT_OPTIONS_FIRST:-false}; then
-      for arg in "${argv[@]}"; do
-        parsed_params+=('a')
-        parsed_values+=("$arg")
-      done
-      break
-    else
-      parsed_params+=('a')
-      parsed_values+=("${argv[0]}")
-      argv=("${argv[@]:1}")
-    fi
-  done
-  local idx
-  if ${DOCOPT_ADD_HELP:-true}; then
-    for idx in "${parsed_params[@]}"; do
-      [[ $idx = 'a' ]] && continue
-      if [[ ${shorts[$idx]} = "-h" || ${longs[$idx]} = "--help" ]]; then
-        stdout "$doc"
-        _return 0
-      fi
-    done
-  fi
-  if [[ ${DOCOPT_PROGRAM_VERSION:-false} != 'false' ]]; then
-    for idx in "${parsed_params[@]}"; do
-      [[ $idx = 'a' ]] && continue
-      if [[ ${longs[$idx]} = "--version" ]]; then
-        stdout "$DOCOPT_PROGRAM_VERSION"
-        _return 0
-      fi
-    done
-  fi
-
-  local i=0
-  while [[ $i -lt ${#parsed_params[@]} ]]; do
-    left+=("$i")
-    ((i++)) || true
-  done
-
-  if ! required "$root_idx" || [ ${#left[@]} -gt 0 ]; then
-    error
-  fi
-  return 0
-}
