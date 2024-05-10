1467f991e3e7e5dbd540fc7f026dd665db91901f
Anders Ingemann <aim@orbit.online>
2024-05-10 09:14:44 +0200

    Update release workflow

--- docopt_sh/__init__.py
+++ docopt_sh/__init__.py
@@ -5,12 +5,14 @@ docopt.sh - Bash argument parser generator.
 """
 # The "... Usage: ..." above contains a zero-width space between `Usage` and `:`
 # in order to prevent docopt from parsing it as a `usage:`` section
+import importlib.metadata
+
+try:
+  __version__ = importlib.metadata.version('docopt-sh')
+except importlib.metadata.PackageNotFoundError:
+  __version__ = '0.0.0-dev'
 
 __all__ = ['docopt_sh']
-try:
-  from .version import __version__
-except ImportError:
-  __version__ = '0.0.0-dev'
 
 
 class DocoptError(Exception):
